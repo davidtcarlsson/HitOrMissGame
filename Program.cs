444c4852a4d4e7f81c60e93d789cc4c6c60fbe32
David Carlsson <carlssondav@gmail.com>
2021-11-25 16:05:26 +0100

    Changed the testArgs

--- Program.cs
+++ Program.cs
@@ -8,9 +8,9 @@ namespace ProjektarbeteV2
         {
             //"1, 1, 5 ; 2, 2, 10" "CIRCLE, 5, 3, 100 ; CIRCLE, 1, 1, 200; " "SQUARE, 5 ; CIRCLE, 10"
             string[] testArgs = {
-                "1, 1,5 ;",
-                "TEST , 5,3 ,10 ;CIRCLE, 1,1, 12;",
-                " SQUARE, 5;CIRCLE,10;"
+                "1, 1,5 ;2,2,10",
+                "SQUARE , 5,3 ,10 ;CIRCLE, 1,1, 12; TRIANGLE, 0, 0, 60;",
+                " TRIANGLE, 3;SQUARE, 5;CIRCLE,10;"
             };
             // "1, 1,5 ;" "SQUARE , 5,3 ,10 ;" " SQUARE, 5;"
 
