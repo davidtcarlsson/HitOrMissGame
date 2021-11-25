fec78f59c75647fd20a869eee5e3426bf88b7124
David Carlsson <carlssondav@gmail.com>
2021-11-25 17:10:32 +0100

    Changed the test arguments

--- Program.cs
+++ Program.cs
@@ -9,8 +9,8 @@ namespace ProjektarbeteV2
             //"1, 1, 5 ; 2, 2, 10" "CIRCLE, 5, 3, 100 ; CIRCLE, 1, 1, 200; " "SQUARE, 5 ; CIRCLE, 10"
             string[] testArgs = {
                 "1, 1,5 ;2,2,10",
-                "SQUARE , 5,3 ,10 ;CIRCLE, 1,1, 12; TRIANGLE, 0, 0, 60;",
-                " TRIANGLE, 3;SQUARE, 5;CIRCLE,10;"
+                "SQUARE , 5,3 ,10 ;CIRCLE, 1,1, 12; TRIANGLE, 0, 0, 60;OCTAGON, 1,-20, 15",
+                " TRIANGLE, 3;SQUARE, 5;OCTAGON, 7;CIRCLE,10;"
             };
             // "1, 1,5 ;" "SQUARE , 5,3 ,10 ;" " SQUARE, 5;"
 
