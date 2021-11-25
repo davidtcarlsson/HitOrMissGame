0322c41c8c0b95d0c50163766f3504205f142d64
David Carlsson <carlssondav@gmail.com>
2021-11-25 13:42:26 +0100

    Added rounding to the final score

--- Program.cs
+++ Program.cs
@@ -9,13 +9,13 @@ namespace ProjektarbeteV2
             //"1, 1, 5 ; 2, 2, 10" "CIRCLE, 5, 3, 100 ; CIRCLE, 1, 1, 200; " "SQUARE, 5 ; CIRCLE, 10"
             string[] testArgs = {
                 "1, 1,5 ;",
-                "SQUARE, 5,3 ,10 ;",
-                "SQUARE, 5;"
+                "TEST , 5,3 ,10 ;CIRCLE, 1,1, 12;",
+                " SQUARE, 5;CIRCLE,10;"
             };
             // "1, 1,5 ;" "SQUARE , 5,3 ,10 ;" " SQUARE, 5;"
 
             HitOrMissGame game = new HitOrMissGame(testArgs);
-            Console.WriteLine(game.GetScore());
+            Console.WriteLine(Math.Round(game.GetScore(), 0));
         }
     }
 }
